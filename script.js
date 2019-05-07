4d717d38dcea45897daf0c95ae3330e652e904bd
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2019-05-06 19:22:02 -0700

    finishing touches

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Search on enter key event
+// Search when enter is pressed
 function search(e) {
     if (e.keyCode === 13) {
         var val = document.getElementById("search-field").value;
@@ -17,22 +17,7 @@ function getTime() {
         (sec < 10 ? ("0" + sec) : sec);
 }
 window.onload = () => {
-    let xhr = new XMLHttpRequest();
-    // Request to open weather map
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=4737316&units=imperial&appid=e5b292ae2f9dae5f29e11499c2d82ece');
-    xhr.onload = () => {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                let json = JSON.parse(xhr.responseText);
-                console.log(json);
-                document.getElementById("temp").innerHTML = json.main.temp.toFixed(0) + " F";
-                document.getElementById("weather-description").innerHTML = json.weather[0].description;
-            } else {
-                console.log('error msg: ' + xhr.status);
-            }
-        }
-    };
-    xhr.send();
+
     // Set up the clock
     document.getElementById("clock").innerHTML = getTime();
     // Set clock interval to tick clock
@@ -41,10 +26,12 @@ window.onload = () => {
     },100);
 };
 document.addEventListener("keydown", event => {
-    if (event.keyCode === 32) {          // Spacebar code to open search
+    if (event.keyCode === 32) {
+        // Spacebar code to open search-field
         document.getElementById('search').style.display = 'flex';
         document.getElementById('search-field').focus();
-    } else if (event.keyCode === 27) {   // Esc to close search
+    } else if (event.keyCode === 27) {
+        // Esc to close search-field
         document.getElementById('search-field').value = '';
         document.getElementById('search-field').blur();
         document.getElementById('search').style.display = 'none';

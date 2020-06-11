ed0691db177a9cc454014381952daf434019f3a0
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-06-10 19:53:40 -0700

    Committed by tlh

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+/*
+ * Evening Startpage
+ * Inspired by https://github.com/jeroenpardon/sui
+ * And https://github.com/TB-96/Evening-Startpage
+ * Made by Thomas Leon Highbaugh
+ * License MIT
+ *
+ */
+
+function startTime() {
+  var currentDate = new Date();
+  var hr = parseInt(currentDate.getHours());
+  var min = parseInt(currentDate.getMinutes());
+  var sec = parseInt(currentDate.getSeconds());
+  //Add a zero in front of numbers<10
+  if (min < 10) {
+    min = "0" + min;
+
+  }
+  if (sec < 10){
+    sec = "0" + sec;
+  }
+
+  document.getElementById("header-time").innerHTML = hr + ":" + min;
+
+  var dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  }
+
+  var date = currentDate.toLocaleDateString("en-US", dateOptions);
+  document.getElementById("header-date").innerHTML = date;
+
+  var time = setTimeout(function(){ startTime() }, 60);
+}
+
+// const quotes = [
+//   'Om Jayanti Mangala Kali \n  Bhadrakali Kapalini \n   Durga Kshama Shiva Dhatri \n   Svaha Svadha Namostuthe.',
+//   'Jaya Bhagavati Devi Namo Vara De\n Jaya Paapa Vinaashini Bahu Phala De \n Jaya Shumbha Nishumbha Kapaala Dhare\n Prannamaami Tu Devi Naraartihare ',
+//   'Jatta Attavii Galaj Jala Pravaaha Paavita Sthale \n   Gale Avalambya Lambitaam Bhujangga Tungga Maalikaam \n   Ddamadd Ddamadd Ddamadd Ddaman Ninaadavadd Ddamar Vayam \n  Cakaara Canndda Taannddavam Tanotu Nah Shivah Shivam',
+// ];
+// document.getElementById("header-quote").innerText = quotes[
+//   Math.floor(Math.random() * quotes.length)
+// ];
+
+$(document).ready(function(){
+  $("#togglebookmarks").click(function(){
+    $("#hiddenbookmarks").toggle();
+  });
+});
+
+document.addEventListener('click', function (event) {
+
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('#link')) return;
+	// Otherwise, run your code...
+	document.body.style.opacity = 0;
+
+}, false);
+
+document.getElementById("container").addEventListener("DOMContentLoaded", startTime());

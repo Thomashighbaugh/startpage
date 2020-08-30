4966687ead2f3b6994d148930a38ad1c38098b5d
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-08-30 00:09:18 -0700

    Committed by tlh

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../styles/Home.module.css";
 
 import styled from "styled-components";
+import { FaGithubAlt } from "react-icons/fa";
 
 const FootWrap = styled.footer`
   width: 100%;
@@ -13,10 +14,20 @@ const FootWrap = styled.footer`
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <a href="https://thomasleonhighbaugh.me" target="_blank">
-        <img src="/logo.png" alt="Vercel Logo" className={styles.logo} />
-      </a>
+    <footer className="row align-content-center justify-content-center ">
+      <div className="col col-6 d-block align-content-center justify-content-center">
+        <a href="https://thomasleonhighbaugh.me" target="_blank">
+          <img src="/logo.png" alt="Vercel Logo" className={styles.logo} />
+        </a>
+        <h6 className="text-white">Thomas Leon Highbaugh</h6>
+      </div>
+
+      <div className="col-6 align-content-center justify-content-center">
+        <a href="https://github.com/THomashighbaugh/startpage">
+          <img src="/github.png" alt="github logo" />{" "}
+        </a>
+        <h6 className="text-white">Source Code</h6>
+      </div>
     </footer>
   );
 }
